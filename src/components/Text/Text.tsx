45c1c35086f4c8eb935658b030bb412c0ee12e9a
Israel Alfaro <israel.devalfaro@gmail.com>
2021-07-10 23:16:23 -0600

    :recycle: Clean code in Text component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import type {TextProps} from './Text.types';
 import {StyledText} from './Text.styles';
+import colors from '../../constants/colors';
 
 export const Text: React.FC<TextProps> = ({
   text,
   style = {},
   fontSize = 12,
-  color = '#FFF',
+  color = colors.black,
 }) => {
   return (
     <StyledText color={color} fontSize={fontSize} style={style}>
